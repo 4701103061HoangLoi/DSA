22557c00b2f4a51f8e146ace96791f535fe90508
Hoàng Tấn Lợi <93914343+4701103061HoangLoi@users.noreply.github.com>
2023-06-18 08:50:22 +0700

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -2,14 +2,13 @@ void quicksort(int* arr, int begin, int end) {
 	if (begin < end)
 	{
 		int pivot = arr[(high + low)/2];
-        		int i = low - 1;
-        		int j = high + 1;
-        		        do {
-            while (arr[++i] < pivot);
-            while (arr[--j] > pivot);
-            if (i < j) std::swap(arr[i], arr[j]);
-        }
-        while (i < j);
+        	int i = low - 1;
+        	int j = high + 1;
+        	do {
+            		while (arr[++i] < pivot);
+            		while (arr[--j] > pivot);
+            		if (i < j) std::swap(arr[i], arr[j]);
+        	} while (i < j);
 		quicksort(arr, begin, j);
 		quicksort(arr, j + 1, end);
 	}
